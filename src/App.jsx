672ee1f6bd7a13f2d34f24f32903fa9902938f84
giahienpham <giahien14604@gmail.com>
2024-07-07 17:24:09 +0700

    add tested buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-     Hello world!
-    </h1>
-  )
-}
-export default App
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem]">
+        <Button className="mt-10" href="#login">
+          Anh iu em
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+export default App;
